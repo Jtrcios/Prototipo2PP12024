ab53cee7d11c33f9354a4c355d4b5f11428edba0
Jtrcios <joseturcios1579@gmail.com>
2024-04-17 10:28:48 +0000

    Login

--- parcial2/main.cpp
+++ parcial2/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include<fstream>
+
+#include "usuarios.h"
+#include "bitacora.h"
+#include "reportes9959224451.h"
+#include "crud9959224451.h"
 
 using namespace std;
-
+void creditosModulo();
+void catalogos();
+void reportes();
+void menuGeneral();
+usuarios usuarioRegistrado;
 int main()
 {
-    cout << "Hello world!" << endl;
+    bool accesoUsuarios;
+    //creditosModulo();
+
+    accesoUsuarios=usuarioRegistrado.loginUsuarios();
+    if (accesoUsuarios){
+        cout<<"Bienvenido";
+    }
+    system("cls");
+    cout<<"** Hasta la proxima **";
     return 0;
 }
